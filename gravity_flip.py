e076f55b9478fd983f551cf81a87dc366f430273
senagray16 <senaderessa00@gmail.com>
2026-02-15 10:01:56 +0300

    Create gravity_flip.py

---
+++ gravity_flip.py
@@ -0,0 +1,17 @@
+x = int (input())
+arr = input().split()
+for i in range(x):
+    arr[i]= int (arr[i])
+    4
+    
+arr.sort()
+
+z= ""
+for i in arr:
+    z = z +  str(i) + " "
+
+print (z)
+    
+
+
+    
